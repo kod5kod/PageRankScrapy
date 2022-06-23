aaecf14509e58a0710e86b7593740d921c400a10
Lee <lee@Lees-MacBook-Pro.local>
2022-06-23 13:28:17 +0300

    removing unnecessary stuff

--- hadoop/src/main/java/DBOutput.java
+++
@@ -1,9 +0,0 @@
-import org.apache.hadoop.io.LongWritable;
-import org.apache.hadoop.io.Text;
-import org.apache.hadoop.mapreduce.Mapper;
-
-public class DBOutput {
-    public static class PageRankMapper extends Mapper<LongWritable, Text, Text, Text> {
-        
-    }
-}
